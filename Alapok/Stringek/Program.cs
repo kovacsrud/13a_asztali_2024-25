dc0717ab7c56417eb65837cbccef3c80a33f0025
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-09-08 09:23:50 +0200

    String műveletek

---
+++ Alapok/Stringek/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg = "ValAmi SzöveG";
+            string szoveg2 = "Valami szöveg";
+
+            Console.WriteLine(szoveg.ToUpper());
+            Console.WriteLine(szoveg.ToLower());
+
+            if (szoveg.ToLower()==szoveg2.ToLower())
+            {
+                Console.WriteLine("Egyenlőek");
+            } else
+            {
+                Console.WriteLine("Nem egyenlőek");
+            }
+
+            Console.WriteLine(szoveg.Contains("ami"));
+            Console.WriteLine(szoveg.StartsWith("Val"));
+            Console.WriteLine(szoveg.EndsWith("Val"));
+
+            Console.ReadKey();
+        }
+    }
+}
