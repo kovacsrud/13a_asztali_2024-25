42555286e19ba7e1df5aad2abc1e2430ee39605b
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-09-21 11:27:55 +0200

    Ciklusok

---
+++ Alapok/ciklusok/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ciklusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciklus - ismételt tevékenységek
+            //Írjunk ki egy bekért szöveget 100x
+            Console.Write("Adj meg egy szöveget:");
+            string szoveg = Console.ReadLine();
+
+            //Előírt lépésszámú
+            for (int i = 0; i < 100; i++)
+            {
+                //ciklusmag
+                Console.WriteLine($"{i+1}.{szoveg}");
+            }
+
+            //Elöltesztelő
+            int cv = 0;
+            while (cv<100)
+            {
+                Console.WriteLine($"{cv+1}.{szoveg}");
+                cv++;
+            }
+
+            //Hátultesztelő - egyszer mindenféleképpen lefut
+            cv = 0;
+            do
+            {
+                Console.WriteLine($"{cv + 1}.{szoveg}");
+                cv++;
+            } while (cv<100);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
