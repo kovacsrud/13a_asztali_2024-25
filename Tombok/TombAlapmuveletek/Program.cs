e7b18edd2eea23ce6a9176e5f5a3fb165f2478a9
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-09-22 11:08:08 +0200

    Update Program.cs

--- Tombok/TombAlapmuveletek/Program.cs
+++ Tombok/TombAlapmuveletek/Program.cs
@@ -35,6 +35,25 @@ namespace TombAlapmuveletek
             Console.WriteLine();
             Console.WriteLine($"Az elemek összege:{osszeg},Átlag:{osszeg/szamok.Length}");
 
+            int min = szamok[0];
+            int max = szamok[0];
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                if (szamok[i]<min)
+                {
+                    min = szamok[i];
+                }
+                if (szamok[i] > max)
+                {
+                    max = szamok[i];
+                }
+            }
+
+            Console.WriteLine($"Min:{min},Max:{max}");
+
+            Console.WriteLine($"Össz:{szamok.Sum()},Átlag:{szamok.Average()},Min:{szamok.Min()},Max:{szamok.Max()}");
+
             Console.ReadKey();
         }
     }
