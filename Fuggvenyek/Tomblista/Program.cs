1155df868436952c55ee3e7fe0c773403e9f39c8
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-10-05 12:19:31 +0200

    Tömb lista

---
+++ Fuggvenyek/Tomblista/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomblista
+{
+    class Program
+    {
+        static void Tomblistazas(int[] tomb)
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.WriteLine(tomb[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] szamok = { 12, 34, 56, 221, 556, 23, 889, 122, 67, 98 };
+
+            //Listázzuk ki az elemeket
+            //for (int i = 0; i < szamok.Length; i++)
+            //{
+            //    Console.WriteLine(szamok[i]);
+            //}
+            Tomblistazas(szamok);
+
+            Console.ReadKey();
+        }
+    }
+}
