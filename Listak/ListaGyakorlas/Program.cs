16b6939bcde1a0c3e8577c86b465c3135c883dd7
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-10-19 11:25:27 +0200

    Gyakorlás

---
+++ Listak/ListaGyakorlas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaGyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hozzon létre egy egész számokat tartalmazó listát!
+            //Töltse fel -100 és 100 közötti véletlen számokkal!
+            //Az hogy hány eleme legyen a listának, szintén legyen véletlenszerű (20-60)
+            //Hozzon létre egy listát a pozitív számok részére!
+            //Helyezze át az eredti listából a pozitív számokat az új listába!
+            //A régi listából távolítsa el a pozitív számokat!
+            //Listázza a két listát
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
