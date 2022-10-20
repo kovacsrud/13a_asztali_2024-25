3c0885edfb69826c78d11777d76b28d4022eb540
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-10-20 09:20:03 +0200

    Lotto játék

---
+++ Lotto/Lotto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int osszSzam = 0;
+            int hanySzam = 0;
+
+            Console.Write("Hány számot húzunk?:");
+            hanySzam = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hány számból sorsolunk?:");
+            osszSzam = Convert.ToInt32(Console.ReadLine());
+
+            int[] tippek = new int[hanySzam];
+            int[] nyeroSzamok = new int[hanySzam];
+
+            //Tippek bekérése
+            //A számnak 1 és osssSzam között kell lennie
+            //Egy tipp csak egyszer szerepelhet a tömbben
+
+            for (int i = 0; i < hanySzam; i++)
+            {
+                Console.Write($"{i + 1}.tipp:");
+                int temp = Convert.ToInt32(Console.ReadLine());
+                while (temp < 1 || temp > osszSzam || tippek.Contains(temp))
+                {
+                    Console.Write($"Rossz! {i + 1}.tipp újra:");
+                    temp = Convert.ToInt32(Console.ReadLine());
+                }
+                tippek[i] = temp;
+            }
+
+            Tomblista(tippek);
+
+            Console.ReadKey();
+        }
+
+        private static void Tomblista(int[] tomb)
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write(tomb[i] + " ");
+            }
+        }
+    }
+}
