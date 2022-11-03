a5d173932799ad9226ddd33ffc2dd5bbc93a6a24
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-11-03 09:05:42 +0100

    Karakterek manipulációja

---
+++ KarakterManipulacio/KarakterManipulacio/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterManipulacio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg = "Valami szöveg";
+            char[] szovegChar = szoveg.ToCharArray();
+
+            for (int i = 0; i < szovegChar.Length; i++)
+            {
+                if (szovegChar[i]=='a')
+                {
+                    szovegChar[i] = 'b';
+                }
+               
+            }
+
+            szoveg = new string(szovegChar);
+            Console.WriteLine(szoveg);
+
+            //A foreach-ben nem lehet módosítani az adatokat!
+            //foreach (var i in szoveg)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
