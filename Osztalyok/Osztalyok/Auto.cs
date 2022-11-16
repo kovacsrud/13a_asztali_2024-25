a186dae1c621888aac896e1cc15d3e9b3be27129
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-11-16 11:30:23 +0100

    Osztaly és oop alapok

---
+++ Osztalyok/Osztalyok/Auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok
+{
+    class Auto
+    {
+       private string rendszam;
+       private string marka;
+       private string tipus;
+       private int gyartasiev;
+
+        //Konstruktor, az osztály példányosításakor automatikusan lefut
+        public Auto(string rendszam,string marka,string tipus,int gyartasiev)
+        {
+            this.rendszam = rendszam;
+            this.marka = marka;
+            this.tipus = tipus;
+            this.gyartasiev = gyartasiev;
+        }
+
+        public void setRendszam(string rendszam)
+        {
+            this.rendszam = rendszam;
+        }
+
+        public string getRendszam()
+        {
+            return rendszam;
+        }
+    }
+}
---
+++ Osztalyok/Osztalyok/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Példányosítás
+            Auto auto1 = new Auto("KLG-001","Skoda","Octavia",2012);
+            Auto auto2 = new Auto("KLX-451", "Ford", "Mondeo", 2022);
+
+            //auto1.rendszam = "KLG-001";
+            //auto1.marka = "Skoda";
+            //auto1.tipus = "Octavia";
+            //auto1.gyartasiev = 2012;
+
+            //Console.WriteLine(auto1.rendszam);
+
+            auto1.setRendszam("KLG-101");
+
+            Console.WriteLine(auto1.getRendszam());
+            Console.WriteLine(auto2.getRendszam());
+
+            Console.ReadKey();
+        }
+    }
+}
