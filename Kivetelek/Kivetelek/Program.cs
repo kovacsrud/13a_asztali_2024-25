de5671b0c599b3c574d95a3838496ae6c6c9f8e1
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-11-16 10:51:53 +0100

    Kivételek

---
+++ Kivetelek/Kivetelek/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetelek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 2;
+            int c = 0;
+
+            try
+            {
+                c = a / b;
+                try
+                {
+                    throw new FormatException("Hibás változó");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);                   
+                }
+              
+                
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Nullával való osztás!");
+            }
+          
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);                
+            }
+            finally
+            {
+                //Mindig lefut az itt lévő kód!
+                Console.WriteLine(c);
+            }
+            
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
