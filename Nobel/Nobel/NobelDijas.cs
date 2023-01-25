4c39966156b373624ca0b0a7988f8ff21302c334
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-01-25 11:20:13 +0100

    Nobel díjasok

---
+++ Nobel/Nobel/NobelDijas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel
+{
+    class NobelDijas
+    {
+        public int Ev { get; set; }
+        public string Tipus { get; set; }
+        public string  Keresztnev { get; set; }
+        public string Vezeteknev { get; set; }
+
+        public NobelDijas(string sor)
+        {
+            var adatok = sor.Split(';');
+            Ev = Convert.ToInt32(adatok[0]);
+            Tipus = adatok[1];
+            Keresztnev = adatok[2];
+            Vezeteknev = adatok[3];
+        }
+    }
+}
---
+++ Nobel/Nobel/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Nobel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<NobelDijas> nobeldijasok = new List<NobelDijas>();
+            try
+            {
+                var sorok = File.ReadAllLines("nobel.csv", Encoding.Default);
+
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    nobeldijasok.Add(new NobelDijas(sorok[i]));
+                }
+
+                Console.WriteLine("Beolvasás kész!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"Adatok száma:{nobeldijasok.Count}");
+
+            var macdonald = nobeldijasok.Find(x => x.Vezeteknev.ToLower() == "mcdonald".ToLower() && x.Keresztnev.ToLower() == "Arthur B.".ToLower());
+
+            if (macdonald != null)
+            {
+                Console.WriteLine(macdonald.Tipus);
+            } else
+            {
+                Console.WriteLine("Nincs ilyen Nobel-díjas!");
+            }
+
+            var irodalmi2017 = nobeldijasok.Find(x => x.Tipus == "irodalmi" && x.Ev == 2017);
+
+            if (irodalmi2017!=null)
+            {
+                Console.WriteLine($"{irodalmi2017.Vezeteknev} {irodalmi2017.Keresztnev}");
+            } else
+            {
+                Console.WriteLine("Nincs ilyen!");
+            }
+
+            var szervezetek = nobeldijasok.FindAll(x => x.Ev >= 1990 && x.Tipus == "béke");
+
+
+            Console.ReadKey();
+        }
+    }
+}
