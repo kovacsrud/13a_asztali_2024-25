471282d27641b1f2293c74448f9ea7eb465dc64f
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-03-08 10:40:30 +0100

    Mentés másként

--- WpfNotepad/WpfNotepad/MainWindow.xaml.cs
+++ WpfNotepad/WpfNotepad/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +36,53 @@ namespace WpfNotepad
         {
             Nevjegy nevjegy = new Nevjegy();
             nevjegy.Show();
+        }
+
+        private void Megnyit_Click(object sender,RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "szöveg (*.txt)|*.txt|adatfájl (*.csv)|*.csv|weblap (*.html)|*.html|minden fájl|*.*";
+
+            if (dialog.ShowDialog()==true)
+            {
+                try
+                {
+                    var szoveg = File.ReadAllText(dialog.FileName, Encoding.Default);
+                    textboxSzoveg.Text = szoveg;
+                    this.Title = dialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+
+        }
+
+        private void MentesMaskent_Click(object sender,RoutedEventArgs e)
+        {
+            MentesMaskent();
+        }
+
+        private void MentesMaskent()
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "szöveg (*.txt)|*.txt|adatfájl (*.csv)|*.csv|weblap (*.html)|*.html|minden fájl|*.*";
+
+            if (dialog.ShowDialog()==true)
+            {
+                try
+                {
+                    File.WriteAllText(dialog.FileName, textboxSzoveg.Text, Encoding.Default);
+                    this.Title = dialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);                    
+                }
+            }
+
         }
     }
 }
