4d5684d30eb4516b981b819b28cf42e791bddfb8
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-05-02 14:02:47 +0200

    Jelszó generátor befejezés

--- WpfJelszoGenerator/WpfJelszoGenerator/MainWindow.xaml.cs
+++ WpfJelszoGenerator/WpfJelszoGenerator/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace WpfJelszoGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        char[] kisBetuk = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+        char[] szamok = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        char[] irasJelek = {'#','@','&','?','_','$','!','%' };
+        Random rand;
         public MainWindow()
         {
             InitializeComponent();
+            rand = new Random();
+        }
+
+        private void buttonGeneral_Click(object sender, RoutedEventArgs e)
+        {
+            List<char> generaltJelszo = new List<char>();
+            List<char> karakterekJelszohoz = new List<char>();
+            if (checkboxKisbetuk.IsChecked==true)
+            {
+                karakterekJelszohoz.AddRange(kisBetuk);
+            }
+            if (checkboxNagybetuk.IsChecked==true)
+            {
+                for (int i = 0; i < kisBetuk.Length; i++)
+                {
+                    karakterekJelszohoz.Add(Char.ToUpper(kisBetuk[i]));
+                }
+            }
+            if (checkboxSzamok.IsChecked==true)
+            {
+                karakterekJelszohoz.AddRange(szamok);
+            }
+            if (checkboxIrasjelek.IsChecked==true)
+            {
+                karakterekJelszohoz.AddRange(irasJelek);
+            }
+
+            if (karakterekJelszohoz.Count>0)
+            {
+                var kivalasztottElem = comboboxHossz.SelectedItem as ComboBoxItem;
+                int hossz = Convert.ToInt32(kivalasztottElem.Content);
+
+                for (int i = 0; i < hossz; i++)
+                {
+                    generaltJelszo.Add(karakterekJelszohoz[rand.Next(0,karakterekJelszohoz.Count)]);
+                }
+
+                textboxJelszo.Text = new string(generaltJelszo.ToArray());
+                Clipboard.SetText(textboxJelszo.Text);
+            }
+            else
+            {
+                MessageBox.Show("Legalább egy négyzetet jelöljön be!");
+            }
+
+            
         }
     }
 }
