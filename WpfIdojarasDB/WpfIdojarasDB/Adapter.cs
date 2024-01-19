541a48b21a23f4a6ed3798f7c26d3e018be1b4ad
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-01-19 10:55:31 +0100

    Adatbázis kezelése

---
+++ WpfIdojarasDB/WpfIdojarasDB/Adapter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfIdojarasDB
+{
+    public class Adapter
+    {
+        SQLiteConnection conn;
+        SQLiteDataAdapter adapter;
+        public DataTable adatok;
+
+        public Adapter(string connstring)
+        {
+            conn = new SQLiteConnection(connstring);
+            adatok = new DataTable();
+
+            adapter = new SQLiteDataAdapter("", conn);
+
+            //select
+            adapter.SelectCommand = new SQLiteCommand(conn);
+            adapter.SelectCommand.CommandText = "select rowid,* from idojarasadatok";
+
+            //insert
+            adapter.InsertCommand = new SQLiteCommand(conn);
+            adapter.InsertCommand.CommandText = "insert into idojarasadatok(ev,honap,nap,ora,homerseklet,szelsebesseg,paratartalom) values(@ev,@honap,@nap,@ora,@homerseklet,@szelsebesseg,@paratartalom)";
+            adapter.InsertCommand.Parameters.Add("@ev", DbType.Int32, 0, "ev");
+            adapter.InsertCommand.Parameters.Add("@honap", DbType.Int32, 0, "honap");
+            adapter.InsertCommand.Parameters.Add("@nap", DbType.Int32, 0, "nap");
+            adapter.InsertCommand.Parameters.Add("@ora", DbType.Int32, 0, "ora");
+            adapter.InsertCommand.Parameters.Add("@homerseklet", DbType.Double, 0, "homerseklet");
+            adapter.InsertCommand.Parameters.Add("@szelsebesseg", DbType.Double, 0, "szelsebesseg");
+            adapter.InsertCommand.Parameters.Add("@paratartalom", DbType.Double, 0, "paratartalom");
+
+            adapter.UpdateCommand = new SQLiteCommand(conn);
+            adapter.UpdateCommand.CommandText = "update idojarasadatok set ev=@ev,honap=@honap,nap=@nap,ora=@ora,homerseklet=@homerseklet,szelsebesseg=@szelsebesseg,paratartalom=@paratartalom where rowid=@old_rowid";
+            adapter.UpdateCommand.Parameters.Add("@ev", DbType.Int32, 0, "ev");
+            adapter.UpdateCommand.Parameters.Add("@honap", DbType.Int32, 0, "honap");
+            adapter.UpdateCommand.Parameters.Add("@nap", DbType.Int32, 0, "nap");
+            adapter.UpdateCommand.Parameters.Add("@ora", DbType.Int32, 0, "ora");
+            adapter.UpdateCommand.Parameters.Add("@homerseklet", DbType.Double, 0, "homerseklet");
+            adapter.UpdateCommand.Parameters.Add("@szelsebesseg", DbType.Double, 0, "szelsebesseg");
+            adapter.UpdateCommand.Parameters.Add("@paratartalom", DbType.Double, 0, "paratartalom");
+            adapter.UpdateCommand.Parameters.Add("@old_rowid", DbType.Int32, 0, "rowid").SourceVersion=DataRowVersion.Original;
+
+            adapter.DeleteCommand = new SQLiteCommand(conn);
+            adapter.DeleteCommand.CommandText = "delete from idojarasadatok where rowid=@old_rowid";
+            adapter.DeleteCommand.Parameters.Add("@old_rowid", DbType.Int32, 0, "rowid").SourceVersion = DataRowVersion.Original;
+
+
+        }
+
+        public DataTable GetData()
+        {
+            adapter.Fill(adatok);
+            return adatok;
+        }
+
+        public void Update()
+        {
+            adapter.Update(adatok);
+        }
+
+    }
+}
---
+++ WpfIdojarasDB/WpfIdojarasDB/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIdojarasDB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Adapter adapter;
+        public MainWindow()
+        {
+            InitializeComponent();
+            adapter=new Adapter("Data Source=idojarasadatok_v2.db;Version=3");
+            datagridIdojaras.ItemsSource = adapter.GetData().DefaultView;
+
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            adapter.Update();
+            datagridIdojaras.Items.Refresh();
+        }
+
+        private void datagridIdojaras_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            e.Row.Background = Brushes.Aqua;
+        }
+    }
+}
