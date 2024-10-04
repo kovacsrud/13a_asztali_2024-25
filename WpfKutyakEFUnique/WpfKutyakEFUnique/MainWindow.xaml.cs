e3c3d08bb1196c0900489d5c2d70b92155f2e9e2
kovacsrud <kovacs.rud@gmail.com>
2024-10-04 10:53:13 +0200

    Entity Framework használata

---
+++ WpfKutyakEFUnique/WpfKutyakEFUnique/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfKutyakEFUnique.mvvm.models;
+using WpfKutyakEFUnique.mvvm.viewmodels;
+using WpfKutyakEFUnique.mvvm.views;
+
+namespace WpfKutyakEFUnique
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new KutyaViewModel();
+        }
+
+        private void menuitemRendeles_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuitemKutyanevek_Click(object sender, RoutedEventArgs e)
+        {
+            var vm=DataContext as KutyaViewModel;
+            KutyanevekView kutyanevek=new KutyanevekView(vm);
+            kutyanevek.ShowDialog();
+            
+
+        }
+
+        private void menuitemKutyafajtak_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfKutyakEFUnique/WpfKutyakEFUnique/mvvm/viewmodels/KutyaViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfKutyakEFUnique.mvvm.models;
+
+namespace WpfKutyakEFUnique.mvvm.viewmodels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class KutyaViewModel
+    {
+        KutyakGoodUniqueContext context;
+        public ObservableCollection<Kutya> Kutyak { get; set; }
+        public ObservableCollection<Kutyanevek> Kutyanevek { get; set; }
+        public ObservableCollection<Kutyafajtak> Kutyafajtak { get; set; }
+
+        public Kutya SelectedKutya { get; set; }
+
+        public KutyaViewModel()
+        {
+            context= new KutyakGoodUniqueContext();
+            context.Kutyas.Load();
+            context.Kutyaneveks.Load();
+            context.Kutyafajtaks.Load();
+            Kutyak=context.Kutyas.Local.ToObservableCollection(); 
+            Kutyafajtak=context.Kutyafajtaks.Local.ToObservableCollection();
+            Kutyanevek=context.Kutyaneveks.Local.ToObservableCollection();
+
+        }
+
+        public void DbMentes()
+        {
+            try
+            {
+                var result=context.SaveChanges();
+                if (result>0)
+                {
+                    MessageBox.Show("Adatok mentve");
+                } else
+                {
+                    MessageBox.Show("Nincs változás");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
