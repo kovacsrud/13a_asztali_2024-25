f58f4a7854a362dd15709cf3ed825f0d541fbb25
kovacsrud <kovacs.rud@gmail.com>
2024-12-11 14:31:06 +0100

    Jegyzet App

---
+++ MauiJegyzet/MauiJegyzet/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiJegyzet.mvvm.view;
+
+namespace MauiJegyzet
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new JegyzetView();
+        }
+    }
+}
