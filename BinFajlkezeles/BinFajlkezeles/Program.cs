22cdf5bcad5fd7d856388fe4ecc6e2e83c16c805
kovacsrud <kovacs.rud@gmail.com>
2025-01-24 15:16:46 +0100

    Bináris fájlkezelés

---
+++ BinFajlkezeles/BinFajlkezeles/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+
+namespace BinFajlkezeles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fájlkezelés binárisan");
+
+            var szovegFajl = File.ReadAllBytes("szinek.txt");
+
+            //foreach (var bajt in szovegFajl)
+            //{
+            //    //Console.Write(bajt+" ");
+            //    Console.Write(Convert.ToString(bajt,2).PadLeft(8,'0')+" ");
+            //}
+
+            //Console.WriteLine(Encoding.UTF8.GetString(szovegFajl));
+
+            var zipFajl = File.ReadAllBytes("MonthyHallGame.zip");
+
+            //foreach (var bajt in zipFajl)
+            //{
+            //    Console.Write(Convert.ToString(bajt,2).PadLeft(8,'0')+" ");
+            //}
+            //Console.WriteLine(Encoding.UTF8.GetString(zipFajl));
+
+            using (MemoryStream ms=new MemoryStream(zipFajl))
+            {
+                using (BinaryReader br=new BinaryReader(ms))
+                {
+                    var signature = br.ReadBytes(4);
+                    Console.WriteLine($"Signature:{BitConverter.ToString(signature)}");
+                    //Console.WriteLine($"Signature:{BitConverter.ToInt32(signature)}");
+                    var version = br.ReadBytes(2);
+                    //Console.WriteLine($"Version:{BitConverter.ToString(version)}");
+                    Console.WriteLine($"Version:{BitConverter.ToInt16(version)}");
+                    var flags= br.ReadBytes(2);
+                    Console.WriteLine($"Flags:{BitConverter.ToString(flags)}");
+                    var compression= br.ReadBytes(2);
+                    Console.WriteLine($"Compression:{BitConverter.ToString(compression)}");
+                    //00-04 seconds, 05-10 minute, 11-15 hour
+                    var modtime = br.ReadBytes(2);
+                    var modtimeInt = BitConverter.ToUInt16(modtime);
+
+                    byte hour = (byte)(modtimeInt >> 12);
+
+                    ushort minute = (ushort)((modtimeInt >> 4) & 0b00001111);
+                    
+                    byte second = (byte)(modtimeInt << 12);
+                    
+                    Console.WriteLine($"Óra:{hour},Perc:{minute}, Másodperc:{second}");
+
+
+
+                    Console.WriteLine($"Modtime:{BitConverter.ToString(modtime)}");
+                    var moddate = br.ReadBytes(2);
+                    Console.WriteLine($"Moddate:{BitConverter.ToString(moddate)}");
+                    var crc = br.ReadBytes(4);
+                    Console.WriteLine($"Crc:{BitConverter.ToString(crc)}");
+                    var compressed = br.ReadBytes(4);
+                    Console.WriteLine($"Compressed:{BitConverter.ToString(compressed)}");
+                    var uncompressed = br.ReadBytes(4);
+                    Console.WriteLine($"Uncompressed:{BitConverter.ToString(uncompressed)}");
+                    var filenameLength = br.ReadBytes(2);
+                    Console.WriteLine($"File name length:{BitConverter.ToInt16(filenameLength)}");
+                    var extraLength = br.ReadBytes(2);
+                    Console.WriteLine($"Extra field length:{BitConverter.ToString(extraLength)}");
+                    var filename = br.ReadBytes(BitConverter.ToInt16(filenameLength));
+                    Console.WriteLine($"Fajlnev:{Encoding.UTF8.GetString(filename)}");
+
+
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BinFajlkezeles/SajatFormatum/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace SajatFormatum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fajlnev = "szinek.txt";
+            var szovegfajl = File.ReadAllBytes(fajlnev);
+            var fajlnevBin = Encoding.UTF8.GetBytes(fajlnev);
+
+            //Fájlformátum:fájlnév/adatok
+            //fájl mérete byteokban:
+            Console.WriteLine(szovegfajl.Length);
+            Console.WriteLine(fajlnevBin.Length);
+
+            var fajlnevHosszBin = BitConverter.GetBytes(fajlnevBin.Length);
+            var fajlhosszBin=BitConverter.GetBytes(szovegfajl.Length);
+
+            //saját fájlformátum: filenév hossza 4/filenév változó/fájl hossza 4/fájl adatok változó
+
+            var binData = new byte[fajlnevHosszBin.Length+fajlnevHosszBin.Length+fajlnevBin.Length+szovegfajl.Length];
+
+            Console.WriteLine(binData.Length);
+
+            using (MemoryStream ms=new MemoryStream(binData))
+            {
+                using (BinaryWriter bw=new BinaryWriter(ms))
+                {
+                    bw.Write(fajlnevHosszBin);
+                    bw.Write(fajlnevBin);
+                    bw.Write(fajlhosszBin);
+                    bw.Write(szovegfajl);
+                }
+                File.WriteAllBytes("szinek.bin",ms.ToArray());
+                Console.WriteLine("Adatok fájlba írva!");
+            }
+
+            //Nyerjük vissza a bin fájlból a szövegfájlt és írjuk ki egy másik nevű szöveges fájlként!
+
+            var binVissza = File.ReadAllBytes("szinek.bin");
+
+            using (MemoryStream ms=new MemoryStream(binVissza))
+            {
+                using (BinaryReader br=new BinaryReader(ms))
+                {
+                    var filenevHosszBin=br.ReadBytes(4);
+                    var filenevHossz = BitConverter.ToInt32(filenevHosszBin);
+                    Console.WriteLine(filenevHossz);
+                    br.ReadBytes(filenevHossz);
+                    var filehosszBin = br.ReadBytes(4);
+                    var filehossz = BitConverter.ToInt32(filehosszBin);
+                    Console.WriteLine(filehossz);
+                    var fileAdatok = br.ReadBytes(filehossz);
+
+                    File.WriteAllBytes("szinek_masolat.txt", fileAdatok);
+                    
+                }
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
