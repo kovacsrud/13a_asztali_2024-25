4aea3689e289a87a64fc4c7e11af9327c43f150b
kovacsrud <kovacs.rud@gmail.com>
2025-03-10 12:33:18 +0100

    Gyakorlás

---
+++ WpfTelekocsi/WpfTelekocsi/Kocsi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTelekocsi
+{
+    public class Kocsi
+    {
+        public string Indulas { get; set; }
+        public string Cel { get; set; }
+        public string Rendszam { get; set; }
+        public string Telefonszam { get; set; }
+        public int Ferohely { get; set; }
+
+        public Kocsi(string sor,char hatarolo)
+        {
+            var adatok = sor.Split(hatarolo);
+
+            Indulas = adatok[0];
+            Cel = adatok[1];
+            Rendszam = adatok[2];
+            Telefonszam = adatok[3];
+            Ferohely = Convert.ToInt32(adatok[4]);
+        }
+    }
+}
---
+++ WpfTelekocsi/WpfTelekocsi/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTelekocsi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Kocsi> Autok { get; set; } = new List<Kocsi>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                var adatok = File.ReadAllLines("telekocsi_autok.csv",Encoding.UTF7);
+
+                for (int i = 1; i < adatok.Length; i++)
+                {
+                    Autok.Add(new Kocsi(adatok[i], ';'));
+                }
+                MessageBox.Show($"Betöltött adatsorok:{Autok.Count}");
+                datagridKocsik.ItemsSource = Autok;
+                comboTelepulesek.ItemsSource = GetTelepulesek();
+                comboTelepulesek.SelectedIndex = 0;
+                comboTelepulesekCel.ItemsSource = GetTelepulesek(true);
+                comboTelepulesekCel.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);                
+            }
+        }
+
+        private void buttonIndulasihelyKeres_Click(object sender, RoutedEventArgs e)
+        {
+            //Itt kell megvalósítani a keresést
+            if (textboxIndulasihely.Text.Length>1)
+            {
+                var result = Autok.FindAll(x=>x.Indulas.ToLower()==textboxIndulasihely.Text.ToLower());
+                if (result.Count>0)
+                {
+                    datagridKocsik.ItemsSource=result;
+                }
+                else
+                {
+                    MessageBox.Show($"Nem található:{textboxIndulasihely.Text}");
+                }
+            } else
+            {
+                MessageBox.Show("Adjon meg egy településnevet!");
+            }
+        }
+
+        private void buttonReset_Click(object sender, RoutedEventArgs e)
+        {
+            datagridKocsik.ItemsSource = Autok;
+        }
+
+        private List<string> GetTelepulesek(bool cel=false)
+        {
+            var telepulesek = new List<string>();
+            if (!cel)
+            {
+                var stat = Autok.ToLookup(x=>x.Indulas);
+                foreach (var i in stat)
+                {
+                    telepulesek.Add(i.Key);
+                }
+            } else
+            {
+                var stat = Autok.ToLookup(x => x.Cel);
+                foreach (var i in stat)
+                {
+                    telepulesek.Add(i.Key);
+                }
+            }
+
+            return telepulesek;
+        }
+    }
+}
